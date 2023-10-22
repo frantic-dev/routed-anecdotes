5d718b59b6428768093ce6b8e1fc6c1cc5d8275d
frantic-dev <okeyamazing@gmail.com>
2023-10-22 18:20:11 +0100

    7.5 anecdotes and hooks step 2 finished

--- src/App.jsx
+++ src/App.jsx
@@ -110,6 +110,13 @@ const CreateNew = props => {
     }, 5000)
   }
 
+  const handleReset = (e) => {
+    e.preventDefault()
+    content.onReset()
+    author.onReset()
+    info.onReset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -139,6 +146,7 @@ const CreateNew = props => {
           />
         </div>
         <button>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   )
